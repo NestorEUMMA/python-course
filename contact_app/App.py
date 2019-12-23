ca1455ab2d8471bade5533d8077bfa7e10c3b10c
NestorEUMMA <ulloa.nestor22@gmail.com>
2019-12-23 16:55:25 -0600

    Insert

--- contact_app/App.py
+++ contact_app/App.py
@@ -1,22 +1,40 @@
 #IMPORTAR LIBRERIAS DE FLASK
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 #IMPORTAR LIBRERIAS DE MYSQL (PARA LA CONEXION)
 from flaskext.mysql import MySQL 
 
+#INICIALIZAMOS FLASK
 app = Flask(__name__)
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'nest2015'
-app.config['MYSQL_DB'] = 'flaskcontacts'
-mysql = MySQL(app)
+#INGRESAMOS LA CONECCION A LA BASE DE DATOS
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'nest2015'
+app.config['MYSQL_DATABASE_DB'] = 'flaskcontacts'
+mysql = MySQL()
+mysql.init_app(app)
 
+#RUTA DEL INDEX
 @app.route('/')
 def Index():
     return render_template('index.html')
 
-@app.route ('/add_contact')
-def add_contact():
-    return 'add contact'
+#RUTA PARA AGREGAR CONTACTOS, DEFINIMOS EL METODO
+@app.route ('/add_contact', methods=['POST'])
+def add_contact(): #DEFINIMOS EL ADD_CONTACT
+    #SI EL REQUEST ES POST, TOMAMOS LOS VALORES DE LOS TEXTBOX
+    if request.method == 'POST':
+        fullname = request.form['fullname']
+        phone = request.form['phone']
+        email = request.form['email']
+        #LLAMAMOS EL CURSOR OBTENER LA CONEXCION
+        cur = mysql.get_db().cursor()
+        #CARGAMOS EL QUERY, LOS PORCENTAJES LOS PONEMOS PARA DESPUES METER UNA TUPLA CON LOS VALORES QUE CAPTURAMOS CON EL METODO POST
+        cur.execute('INSERT INTO contacts (fullname, phone, email) VALUES (%s, %s, %s)',
+        (fullname, phone, email))
+        #HACEMOS UN COMMIT PARA QUE LOS VALORES INGRESEN A LA BASE DE DATOS.
+        mysql.get_db().commit()
+        return 'received'
+    
 
 @app.route ('/edit')
 def edit_contact():
@@ -26,5 +44,6 @@ def edit_contact():
 def delete_contact():
     return 'delete contact'       
 
+#CONFIGURAMOS EL PUERTO, LEVANTAMOS EL SERVIDOR Y EL MODO DEBUG
 if  __name__ == '__main__':
     app.run(port = 3000, debug = True)
