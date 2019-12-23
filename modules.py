a6a572a88f6b37e270a84fab0a653e396305d36f
NestorEUMMA <ulloa.nestor22@gmail.com>
2019-12-23 13:09:26 -0600

    Finalizacion de Curso

--- modules.py
+++ modules.py
@@ -8,6 +8,11 @@
 #fmath.substract(1,2)
 
 
-from fmath import add, substract
-substract(1,2)
-add(1,2)
+#from fmath import add, substract
+#substract(1,2)
+#add(1,2)
+
+
+from colorama import Fore, Style, init
+init(convert=True)
+print(Fore.RED + "Hello World")
